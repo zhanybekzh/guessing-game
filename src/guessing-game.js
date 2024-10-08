03477d5f0dac4a5a1051d042d72a45808d2c7555
Zhandos <zh.zhanybek@gmail.com>
2024-10-08 12:56:10 +0500

    feat: write class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.currentGuess = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.currentGuess = Math.round((this.min + this.max) / 2);
+    return this.currentGuess;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.currentGuess;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.currentGuess;
+  }
 }
 
 module.exports = GuessingGame;
